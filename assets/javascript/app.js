ec6e702e51c543eeb0ab5e45aae155d88bb81d2c
vpdsgnz <vpdsgnz@hotmail.com>
2019-06-05 09:23:40 -0400

    minor changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,52 +8,52 @@ $(document).ready(function(){
 	
 var myQuestions = [
 	{
-		question: "How did Daenerys Targaryen eventually hatch her dragon eggs?", 
+		question: "1. How did Daenerys Targaryen eventually hatch her dragon eggs?", 
 		choices: ["In a lightning storm", "In a funeral pyre", "In a fireplace", "In a frozen cave"],
 		answer: 1,
 	},
 	{
-		question: "Which Stark family direwolf was killed in retaliation for an attack on Prince Joffrey?", 
+		question: "2. Which Stark family direwolf was killed in retaliation for an attack on Prince Joffrey?", 
 		choices: ["Ghost", "Lady", "Nymeria", "Summer"],
 		answer: 1,
 	},
 	{
-		question: "The phrase 'Valar Morghulis' or 'all men must die' is usually responded with:", 
+		question: "3. The phrase 'Valar Morghulis' or 'all men must die' is usually responded with:", 
 		choices: ["Valar Dohaeris or 'all men must serve'", "Valar Rohnas or 'all men must live'", "Valar GoGo or 'all men must dance'"],
 		answer: 0,
 	},
 	{
-		question: "'It's nothing' were the last words of this infamous character:", 
+		question: "4. 'It's nothing' were the last words of this infamous character:", 
 		choices: ["Renly Baratheon", "Tywin Lannister", "Robb Stark", "King Joffrey"],
 		answer: 3,
 	},
 	{
-		question: "What is the only thing that can put out volatile Wildfire?", 
+		question: "5. What is the only thing that can put out volatile Wildfire?", 
 		choices: ["Sand", "Water", "Dragon's blood", "Sunlight"],
 		answer: 0,
 	},
 	{
-		question: "Besides dragonglass, what is the only other substance capable of defeating White Walkers?", 
+		question: "6. Besides dragonglass, what is the only other substance capable of defeating White Walkers?", 
 		choices: ["Weirwood", "Wildfire", "Valyrian Steel", "Snowballs"],
 		answer: 2,
 	},
 	{
-		question: "How many times has Beric Dondarrion been brought back to life?", 
+		question: "7. How many times has Beric Dondarrion been brought back to life?", 
 		choices: ["Three times", "Five times", "Six times", "Seven times"],
 		answer: 2,
 	},
 	{
-		question: "Who shoots the flaming arrow that subsequently destroy's Stannis' fleet in Blackwater Bay?", 
+		question: "8. Who shoots the flaming arrow that subsequently destroy's Stannis' fleet in Blackwater Bay?", 
 		choices: ["Tyrion Lannister", "King Joffrey", "Jaime Lannister", "Bronn"],
 		answer: 3,
 	},
 	{
-		question: "Arya's punishment for stealing from the Many-Face God is:", 
+		question: "9. Arya's punishment for stealing from the Many-Face God is:", 
 		choices: ["Death", "Memory Loss", "Blindness", "Uncontrollable laughter"],
 		answer: 2,
 	},
 	{
-		question: "What was the name of Ned Stark's greatsword?", 
+		question: "10. What was the name of Ned Stark's greatsword?", 
 		choices: ["Ice", "Oathkeeper", "Widow's Wail", "Northguard"],
 		answer: 0,
 	},
@@ -66,7 +66,8 @@ var timer = 10;
 var intervalId;
 var running = false;
 runTimer();
-displayQuestion();
+// displayQuestion();
+buidlQuiz();
 
 function runTimer(){
 	if (!running) {
@@ -92,13 +93,15 @@ function stop() {
 	clearInterval(intervalId);
 }
 
-function displayQuestion() {
-	let current = myQuestions;
-	console.log(current);
-	for (var i = 0; i < myQuestions.length; i++) {
-		$("#quiz").html("<p>" + current.question[i] + "</p>");
-		console.log(current.question[i]);
-	}
+function buidlQuiz() {
+
+	myQuestions.forEach(function(q){
+
+		$("#quiz").append("<h2>" + q.question + "</h2></br>");
+		// $("#quiz").append('<input type="radio" name="question" value="1">'+q.choices[options]);
+		// console.log(q.choices);
+		// console.log(q.answer);
+	})
 }
 
 });
